b49551d1542d5299bba1e27aaee7f7136d1a6690
zaxwebs <zifidigital@gmail.com>
2023-04-08 04:38:02 +0530

    added Prettier config

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,26 @@
+<div class="container py-5">
+	<h1>Get in touch</h1>
+	<p>We look forward to hearing from you.</p>
+	<form>
+		<div class="mb-3">
+			<label for="name" class="form-label">Name</label>
+			<input id="name" type="text" class="form-control" />
+		</div>
+		<div class="mb-3">
+			<label for="email" class="form-label">Email address</label>
+			<input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+			<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+		</div>
+		<div class="mb-3">
+			<label for="message" class="form-label">Message</label>
+			<textarea class="form-control" id="message" rows="3" />
+		</div>
+		<div class="mb-3 form-check">
+			<input type="checkbox" class="form-check-input" id="exampleCheck1" />
+			<label class="form-check-label" for="exampleCheck1">
+				I agree with the terms and conditions and privacy policy.
+			</label>
+		</div>
+		<button type="submit" class="btn btn-primary">Send Message</button>
+	</form>
+</div>
